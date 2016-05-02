4f13e78f6a7773066cc7ea9d05e7ef4c44d4082b
Dan McGee <dpmcgee@gmail.com>
2016-05-01 19:11:42 -0500

    Add ability for manual centering and distance override
    If one wants to produce a series of images over time, for example, it is
    best if the image projection and limits don't keep changing.

--- draw_graph.py
+++ draw_graph.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import math
 from statistics import mean
 import sys
 
@@ -32,14 +33,41 @@ def extract_positions(data):
     return lats, lons, alts
 
 
-def draw_map(lats, lons, px_size, resolution):
+def calculate_limits(lats, lons, center_lat, center_lon, distance):
+    # returns a 6-tuple for helping plot a map:
+    # (lower-left lon, lower-left lat,
+    #  upper-left lon, upper-right lat,
+    #  center lon, center lat)
+    if center_lat is None or center_lon is None:
+        print('using calculated map limits and center')
+        return (min(lons), min(lats),
+                max(lons), max(lats),
+                mean(lons), mean(lats))
+    if distance is None:
+        print('using calculated map limits, provided center')
+        return (min(lons), min(lats),
+                max(lons), max(lats),
+                center_lon, center_lat)
+    else:
+        print('using provided map limits and center')
+        # 1 degree of latitude = ~ 60 nautical miles
+        lat_offset = distance / 60
+        # longitude requires slightly more work
+        lon_offset = distance / (math.cos(math.radians(center_lat)) * 60)
+        return (center_lon - lon_offset,
+                center_lat - lat_offset,
+                center_lon + lon_offset,
+                center_lat + lat_offset,
+                center_lon, center_lat)
+
+
+def draw_map(lats, lons, limits, px_size, resolution):
     print('drawing map...')
     fig_size = px_size // 100
     figure = matplotlib.pyplot.figure(figsize=(fig_size, fig_size))
-    basemap = Basemap(llcrnrlon=min(lons), llcrnrlat=min(lats),
-                      urcrnrlon=max(lons), urcrnrlat=max(lats),
-                      lon_0=mean(lons), lat_0=mean(lats),
-                      resolution='i', projection='tmerc')
+    basemap = Basemap(limits[0], limits[1], limits[2], limits[3],
+                      lon_0=limits[4], lat_0=limits[5],
+                      resolution=resolution, projection='tmerc')
     print('drawing map features...')
     basemap.drawcoastlines()
     basemap.drawstates()
@@ -69,19 +97,26 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('input', help='path to input CSV data, - for stdin')
     parser.add_argument('-o', '--output', help='path to output image', default='map.png')
-    parser.add_argument('--resolution', help='resolution of basemap', default='h',
-                        choices=('c', 'l', 'i', 'h', 'f'))
     parser.add_argument('-s', '--size', help='size of produced image in pixels', default=4000,
                         type=int)
+    parser.add_argument('--resolution', help='resolution of basemap features',
+                        default='h', choices=('c', 'l', 'i', 'h', 'f'))
+    parser.add_argument('--lat', help='latitude for center of map projection', type=float)
+    parser.add_argument('--lon', help='longitude for center of map projection', type=float)
+    parser.add_argument('--distance', help='distance in NM from center to plot', type=float)
     args = parser.parse_args()
 
     import_matplotlib()
+
+    # allow using stdin; helpful for filtering input before plotting
     if args.input == '-':
         lats, lons, alts = extract_positions(sys.stdin)
     else:
         with open(args.input, 'r') as data:
             lats, lons, alts = extract_positions(data)
-    figure, basemap = draw_map(lats, lons, args.size, args.resolution)
+
+    limits = calculate_limits(lats, lons, args.lat, args.lon, args.distance)
+    figure, basemap = draw_map(lats, lons, limits, args.size, args.resolution)
     plot_points(basemap, lats, lons, alts)
     save_image(figure, args.output)
 
